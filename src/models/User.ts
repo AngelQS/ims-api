94972c16473b4c47e9adad478f618cb34e6beac7
AngelQS <aedwin.acuario31@gmail.com>
2020-05-24 20:30:50 -0500

    Create mongoose User model

---
+++ src/models/User.ts
@@ -0,0 +1,19 @@
+// Third
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+model("User", UserSchema);
