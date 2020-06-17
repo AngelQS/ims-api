9fbdc183d719f4e5d92285411dd1c614fc15ea37
AngelQS <aedwin.acuario31@gmail.com>
2020-06-16 19:16:11 -0500

    Added getUserProfile to UserMiddlewares

--- src/middlewares/user.middlewares.ts
+++ src/middlewares/user.middlewares.ts
@@ -1,7 +1,32 @@
 // Third
 import { Request, Response, NextFunction } from "express";
 
-class UserMiddlewares {}
+// Local
+import User from "../models/User";
+import Post from "../models/Post";
+
+class UserMiddlewares {
+  public async getUserProfile(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { _id: userId } = req.params;
+
+      const user = await User.findOne({ _id: userId }).select("-password");
+
+      if (!user) {
+        return Error("User not found");
+      }
+
+      const posts = await Post.find({ postedBy: userId }).populate(
+        "postedBy",
+        "_id name"
+      );
+
+      return res.json({ user, posts });
+    } catch (err) {
+      console.log("Error:", err);
+    }
+  }
+}
 
 const userMiddlewares = new UserMiddlewares();
 
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,14 @@
 // Third
 import { Router } from "express";
 
+// Local
+import UserMiddlewares from "../middlewares/user.middlewares";
+import AuthMiddlewares from "../middlewares/auth.middlewares";
+
+// Initialization
+const { requiresAuthorization } = AuthMiddlewares;
+const { getUserProfile } = UserMiddlewares;
+
 class UserRoutes {
   router: Router;
 
@@ -10,10 +18,10 @@ class UserRoutes {
   }
 
   routes() {
-    // TODO: user routes
+    this.router.get("/:_id", requiresAuthorization, getUserProfile);
   }
 }
-  
+
 const userRoutes = new UserRoutes();
 
 export default userRoutes.router;
